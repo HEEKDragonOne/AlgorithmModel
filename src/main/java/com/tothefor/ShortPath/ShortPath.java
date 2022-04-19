c0252ca7143e5cb3dc0b04dae1d68750b1b1a4ac
HEEKDragonOne <1425138134@qq.com>
2022-04-19 23:26:18 +0800

    2022.4.19 23:24 首次构思此框架和提交基本框架

---
+++ src/main/java/com/tothefor/ShortPath/ShortPath.java
@@ -0,0 +1,10 @@
+package com.tothefor.ShortPath;
+
+/**
+ * @Author DragonOne
+ * @Date 2022/4/19 23:21
+ * @墨水记忆 www.tothefor.com
+ */
+public interface ShortPath {
+    public int Dijkstra();
+}
---
+++ src/main/java/com/tothefor/ShortPath/ShortPathImpl.java
@@ -0,0 +1,13 @@
+package com.tothefor.ShortPath;
+
+/**
+ * @Author DragonOne
+ * @Date 2022/4/19 23:22
+ * @墨水记忆 www.tothefor.com
+ */
+public class ShortPathImpl implements ShortPath{
+    @Override
+    public int Dijkstra() {
+        return 0;
+    }
+}
