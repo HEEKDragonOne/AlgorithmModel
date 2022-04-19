8cf68797da4910a155adef5111a05a82647c45d1
HEEKDragonOne <1425138134@qq.com>
2022-04-19 23:50:04 +0800

    2022.4.19 添加readme

---
+++ src/main/java/com/tothefor/TestAl.java
@@ -0,0 +1,13 @@
+package com.tothefor;
+
+
+/**
+ * @Author DragonOne
+ * @Date 2022/4/19 23:32
+ * @墨水记忆 www.tothefor.com
+ */
+public class TestAl {
+    public static void main(String[] args) {
+
+    }
+}
