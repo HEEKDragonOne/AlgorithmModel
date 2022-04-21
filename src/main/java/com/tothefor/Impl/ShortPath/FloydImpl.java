d3a97a95d7bbda94d112fd77c29ef7df92bd01f9
HEEKDragonOne <1425138134@qq.com>
2022-04-21 14:21:04 +0800

    2022.4.21 完成最短路算法模板

--- src/main/java/com/tothefor/Impl/ShortPath/FloydImpl.java
+++ src/main/java/com/tothefor/Impl/ShortPath/FloydImpl.java
@@ -1,5 +1,7 @@
 package com.tothefor.Impl.ShortPath;
 
+import com.tothefor.ConstEntity.Edge;
+
 /**
  * @Author DragonOne
  * @Date 2022/4/20 16:11
@@ -8,7 +10,74 @@ package com.tothefor.Impl.ShortPath;
  * @English Floyd
  */
 public class FloydImpl {
-    private int indexMax = 200000 + 15;
+    private int indexMax = 1000 + 15;
+    private int INF = 0x3f3f3f3f;
     private int[][] dp = new int[indexMax][indexMax];
+
+    /**
+     * @Chinese 初始化.
+     * @English init.
+     * @since Algorithm 1.0
+     */
+    public void init(int n){
+        for(int i=1;i<=n;++i){
+            for(int j=1;j<=n;++j){
+                dp[i][j]=INF;
+            }
+        }
+    }
+
+    /**
+     * @Chinese 添加关系.
+     * @English Add the relationship.
+     * @since Algorithm 1.0
+     */
+    public void add_edge(int a,int b,int c){
+        dp[a][b] = Math.min(dp[a][b],c);
+    }
+
+    /**
+     * @Chinese Floyd.
+     * @English Floyd.
+     * @since Algorithm 1.0
+     */
+    public int floyd(int n, int start, int end, Edge[] edges){
+        init(n);
+        for (int i = 0; i < edges.length; ++i) {
+            add_edge(edges[i].begin, edges[i].end, edges[i].weight);
+        }
+        for(int k=1;k<=n;++k){
+            for(int i=1;i<=n;++i){
+                for(int j=1;j<=n;++j){
+                    if(dp[i][j]>dp[i][k]+dp[k][j]){
+                        dp[i][j]=dp[i][k]+dp[k][j];
+                    }
+                }
+            }
+        }
+        return dp[start][end]==INF? -1:dp[start][end];
+    }
+    /**
+     * @Chinese Floyd.
+     * @English Floyd.
+     * @since Algorithm 1.0
+     */
+    public int[][] floyd(int n,Edge[] edges){
+        init(n);
+        for (int i = 0; i < edges.length; ++i) {
+            add_edge(edges[i].begin, edges[i].end, edges[i].weight);
+        }
+        for(int k=1;k<=n;++k){
+            for(int i=1;i<=n;++i){
+                for(int j=1;j<=n;++j){
+                    if(dp[i][j]>dp[i][k]+dp[k][j]){
+                        dp[i][j]=dp[i][k]+dp[k][j];
+                    }
+                }
+            }
+        }
+        return dp;
+    }
+
 
 }
--- src/main/java/com/tothefor/Impl/ShortPath/ShortPath.java
+++ src/main/java/com/tothefor/Impl/ShortPath/ShortPath.java
@@ -13,14 +13,16 @@ public final class ShortPath {
 
     private static DijkstraSimpleImpl dijkstraSimple = new DijkstraSimpleImpl();
     private static DijkstraHeapImpl dijkstraHeap = new DijkstraHeapImpl();
+    private static SPFAImpl spfa = new SPFAImpl();
+    private static FloydImpl floyd = new FloydImpl();
 
     /**
      * @Chinese 朴素版Dijkstra.
      * @English Simple version of Dijkstra.
      * @since Algorithm 1.0
      */
-    public static long getSimpleDis(int n, int m, int start, int end, Edge[] edges){
-        return dijkstraSimple.Dijkstra(n,m,start,end,edges);
+    public static long getSimpleDis(int n, int m, int start, int end, Edge[] edges) {
+        return dijkstraSimple.Dijkstra(n, m, start, end, edges);
     }
 
     /**
@@ -28,8 +30,37 @@ public final class ShortPath {
      * @English Heap optimized version of Dijkstra.
      * @since Algorithm 1.0
      */
-    public static long getHeapDis(int n,int m,int start,int end,Edge[] edges){
-        return dijkstraHeap.Dijkstra(n,m,start,end,edges);
+    public static long getHeapDis(int n, int m, int start, int end, Edge[] edges) {
+        return dijkstraHeap.Dijkstra(n, m, start, end, edges);
     }
+
+    /**
+     * @Chinese SPFA判断负环和求最短路，存在负环则返回-1，否则返回最短路.
+     * @English SPFA determines the negative loop and calculates the shortest circuit.
+     * If there is a negative loop, -1 is returned; otherwise, the shortest circuit is returned.
+     * @since Algorithm 1.0
+     */
+    public static int SPFA(int n, int m, int start, int end, Edge[] edges) {
+        return spfa.SPFA(n, m, start, end, edges);
+    }
+
+    /**
+     * @Chinese Floyd求指定两点的最短距离.
+     * @English Floyd finds the shortest distance between a given two points.
+     * @since Algorithm 1.0
+     */
+    public static int[][] Floyd(int n, Edge[] edges) {
+        return floyd.floyd(n, edges);
+    }
+
+    /**
+     * @Chinese Floyd求所有点之间的距离，并返回距离数组.
+     * @English Floyd finds the distance between all points and returns an array of distances.
+     * @since Algorithm 1.0
+     */
+    public static int Floyd(int n, int start, int end, Edge[] edges) {
+        return floyd.floyd(n, start, end, edges);
+    }
+
 
 }
